5fac93170a58a5699789b87a253b44ab48428a12
Aditi Markale <aditimarkale1@gmail.com>
2017-10-03 23:07:53 -0400

    Updating main.php

--- main.php
+++ main.php
@@ -2,11 +2,12 @@
     // Get the PHP helper library from twilio.com/docs/php/install
     require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
     use Twilio\Rest\Client;
-    $account_sid = 'AC6f189af575538e547c0c8bc566ecebba';
-    $auth_token = '73e55146dfda04d73a2e28deba19a417';
-    $client = new Client($account_sid, $auth_token);
-    $messages = $client->messages->create('+12678108571', array(
-         'From' => '+12017201388',
-         'Body' => 'Hello'
-    ));
+    $account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407'; 
+    $auth_token = '6a7fed7f68a9d89d75996bc165982d2b'; 
+    $client = new Client($account_sid, $auth_token); 
+ 
+    $messages = $client->messages->create("+12678108571", array( 
+        'From' => "+12674407881",  
+        'Body' => "Hello aditi",      
+  ));
 ?>
